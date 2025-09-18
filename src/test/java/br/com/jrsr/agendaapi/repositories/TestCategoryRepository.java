868ac3ffca51e323f59eb94c4b5b304f4a618b12
João Rezende <joaoricardorezende@gmail.com>
2025-09-18 19:42:14 -0300

    test: add TaskTest and CategoryTest

---
+++ src/test/java/br/com/jrsr/agendaapi/repositories/TestCategoryRepository.java
@@ -0,0 +1,12 @@
+package br.com.jrsr.agendaapi.repositories;
+
+import br.com.jrsr.agendaapi.domain.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TestCategoryRepository extends JpaRepository<Category, UUID> {
+    Category findByName(String name);
+}
