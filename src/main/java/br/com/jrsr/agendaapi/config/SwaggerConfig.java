60382fe55e5eb1d2b8d1d9915cc1e10d7429d40e
João Rezende <joaoricardorezende@gmail.com>
2025-09-18 18:56:36 -0300

    feat(config): add CORS and Swagger UI configurations

---
+++ src/main/java/br/com/jrsr/agendaapi/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package br.com.jrsr.agendaapi.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI taskSchedulingOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Scheduling API")
+                        .version("1.0.0")
+                        .description("API for managing tasks")
+                        .contact(new Contact()
+                                .name("João Rezende")
+                                .email("joao@example.com")
+                        ))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Complete Documentation")
+                        .url("https://github.com/jrsrezende/task-scheduling-api"));
+    }
+}
+
