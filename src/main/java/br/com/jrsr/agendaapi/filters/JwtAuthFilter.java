fa76176e28fd1db9ef1d7d33ba9af3a6710d5f4b
João Rezende <joaoricardorezende@gmail.com>
2025-10-27 18:22:46 -0300

    refactor: make JwtAuthFilter extend OncePerRequestFilter

--- src/main/java/br/com/jrsr/agendaapi/filters/JwtAuthFilter.java
+++ src/main/java/br/com/jrsr/agendaapi/filters/JwtAuthFilter.java
@@ -1,56 +1,63 @@
 package br.com.jrsr.agendaapi.filters;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
 import java.io.IOException;
 import java.security.Key;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.ServletException;
-import org.springframework.web.filter.GenericFilterBean;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
 
-public class JwtAuthFilter extends GenericFilterBean {
-    private final String secretKey;
-
-    public JwtAuthFilter(String secretKey) {
-        this.secretKey = secretKey;
-    }
+    @Value("${jwt.key}")
+    private String secretKey;
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
-        final HttpServletRequest request = (HttpServletRequest) servletRequest;
-        final HttpServletResponse response = (HttpServletResponse) servletResponse;
-
-        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) { //O navegador primeiro envia uma requisição OPTIONS “preflight” para verificar se a API permite essa operação.
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
             filterChain.doFilter(request, response);
             return;
         }
 
         final String authHeader = request.getHeader("Authorization");
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) { //verifica se o header existe e se começa com Bearer
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized access.");
             return;
         }
 
         final String token = authHeader.substring(7);
         try {
-            Key key = Keys.hmacShaKeyFor(secretKey.getBytes()); //cria a chave de assinatura
-            Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody(); //valida o token e extrai os claims
-            request.setAttribute("claims", claims); //coloca os claims na request para ser usado depois
-            filterChain.doFilter(request, response); // continua a execução da requisição
+            Key key = Keys.hmacShaKeyFor(secretKey.getBytes());
+            Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+
+            request.setAttribute("claims", claims);
+            filterChain.doFilter(request, response);
+
         } catch (ExpiredJwtException e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Expired Token");
         } catch (JwtException | IllegalArgumentException e) {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Token");
         }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getServletPath();
+        return path.startsWith("/swagger-ui") || path.startsWith("/v3/api-docs");
     }
 }
